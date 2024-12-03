669862c870179068bee9da1a4b8aaa2e7ddb783a
Shaddy <shadybett540@gmail.com>
2024-12-03 21:26:30 +0300

    recap on last day's lessons

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,75 @@
+// import 'package:flutter/material.dart';
+
+// void main() => runApp(MaterialApp(
+//       home: Home(),
+//     ));
+
+// class Home extends StatelessWidget {
+//   const Home({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//           title: Text('Carpe Diem',
+//               style: TextStyle(
+//                 color: Colors.grey[200],
+//                 fontSize: 20.0,
+//                 fontWeight: FontWeight.bold,
+//                 fontFamily: 'PlayfairDisplay',
+//               )),
+//           centerTitle: true,
+//           backgroundColor: Colors.blue[600],
+//         ),
+//         body: Row(
+//           mainAxisAlignment: MainAxisAlignment.start,
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           children: [
+//             Expanded(
+//               flex: 3,
+//               child: Image.asset('girl.jpeg')
+//               ),
+//             Expanded(
+//               flex: 1,
+//               child: Container(
+//                 padding: EdgeInsets.all(30.0),
+//                 color: Colors.cyan,
+//                 child: Text('1'),
+//               ),
+//             ),
+//             Expanded(
+//               flex: 1,
+//               child: Container(
+//                 padding: EdgeInsets.all(30.0),
+//                 color: Colors.pinkAccent,
+//                 child: Text('2'),
+//               ),
+//             ),
+//             Expanded(
+//               flex: 1,
+//               child: Container(
+//                 padding: EdgeInsets.all(30.0),
+//                 color: Colors.amber,
+//                 child: Text('3'),
+//               ),
+//             )
+//           ],
+//         ),
+//         floatingActionButton: FloatingActionButton(onPressed: (){},
+//         backgroundColor: Colors.blue[600],
+//         child: Text('Click',style: TextStyle(color: Colors.white),),
+//         ),
+//         );
+//   }
+// }
+
+
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Home(),
-    ));
+void main()=>runApp(MaterialApp(
+  home: Home(),
+));
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -10,33 +77,10 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Carpe Diem',
-              style: TextStyle(
-                color: Colors.grey[200],
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'PlayfairDisplay',
-              )),
-          centerTitle: true,
-          backgroundColor: Colors.blue[600],
-        ),
-        body: Row(
-          children: [
-            Text('Hello world')
-            Container(
-              
-            )
-          ],
-        ),
-
-
-
-        floatingActionButton: FloatingActionButton(onPressed: (){},
-        backgroundColor: Colors.blue[600],
-        child: Text('Click',style: TextStyle(color: Colors.white),),
-        ),
-        );
+      appBar: AppBar(
+        title: Text('Carpe Diem'),
+        centerTitle: true,
+      ),
+    );
   }
 }
-
