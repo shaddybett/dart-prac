d81b7243aa4b1ebce115a4880e6a786c27c25939
Shaddy <shadybett540@gmail.com>
2025-01-10 12:40:42 +0300

    made dynamic route

---
+++ lib/authentication/otp.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Otp extends StatelessWidget {
+  const Otp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('This is otp'),
+      ),
+      body: Center(
+        child: Text('Hello'),
+      ),
+    );
+  }
+}
--- lib/router/approuter.dart
+++ lib/router/approuter.dart
@@ -1,1 +1,27 @@
-import 'package: '
+import 'package:flutter/material.dart';
+import '../authentication/phone.dart';
+import '../authentication/otp.dart';
+
+class AppRouter extends StatelessWidget {
+  const AppRouter({super.key});
+  static const String phone = "/phone";
+  static const String otpscreen = "/otp";
+
+  static Route<dynamic> onGenerateRoute(RouteSettings settings) {
+    final args = settings.arguments;
+
+    switch (settings.name) {
+      case phone:
+        return _slideRoute(PhoneScreen());
+      case otpscreen:
+        return _slideRoute(OtpScreen(verificationCode: args as String));
+      default:
+        return _slideRoute(
+          Scaffold(
+            body: Center(child: Text('Page not found')),
+          ),
+        );
+    }
+  }
+  
+}
