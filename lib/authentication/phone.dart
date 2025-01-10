ec95db6571850e2aa16682e947fb7ef35ff4771b
Shaddy <shadybett540@gmail.com>
2025-01-10 10:57:37 +0300

    done

---
+++ lib/authentication/phone.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: ElevatedButton(onPressed: () {}, child: Text('Go to About')),
+      ),
+    );
+  }
+}
