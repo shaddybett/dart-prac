e1234df49f692824cf9cd016e709464f3f1cb5c5
Shaddy <shadybett540@gmail.com>
2025-01-12 23:41:33 +0300

    loaded otp.dart

--- lib/authentication/otp.dart
+++ lib/authentication/otp.dart
@@ -1,17 +1,30 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:grocery_app/utils/widgets/authentication/otpField.dart';
+
+import '../../router/approuter.dart';
+import '../../utils/widgets/authentication/authpages.dart';
 
 class OtpScreen extends StatelessWidget {
   final String verificationCode;
-  const OtpScreen({Key? key, required this.verificationCode}) : super(key: key);
+  const OtpScreen({super.key, required this.verificationCode});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('OTP Screen'),
-      ),
-      body: Center(
-        child: Text('Verification Code: $verificationCode'),
+    return AuthPageManager(
+      pagetitle: 'register.verify_phone'.tr(),
+      onButtonPressed: () {
+        Navigator.pushNamed(context, AppRouter.kycpage);
+      },
+      buttontext: "home.continue".tr(),
+      pagedescription: "register.enter_otp".tr(),
+      children: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height * .6,
+          child: Column(
+            children: [OTPField(verificationId: verificationCode)],
+          ),
+        ),
       ),
     );
   }
